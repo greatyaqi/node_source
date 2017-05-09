be6b8a5a240b31e0d19cef1e2ae510774b9d34d0
Dave <greatyaqi@163.com>
2017-05-09 18:46:59 +0800

    add redis support

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const Koa = require('koa');
 const Bodyparser = require('koa-bodyparser');
 const db = require('./lib/db')
+const  redis = require('./lib/redis');
+
 
 const app = new Koa();
 
